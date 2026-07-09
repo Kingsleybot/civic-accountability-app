0e986ad49a24e4aa8a2a41b311327c93efafb178
Kingsley Osei-Acheampong <kingsley.ko10@gmail.com>
2026-07-09 09:36:00 +0100

    Fix serverless timeout by conditionally starting listener

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ app.get('/api/officials', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
